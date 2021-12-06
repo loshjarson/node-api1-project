cebba8e06e37e68d7bbcb218cd9d54619f2ee9e1
unknown <jlarson129@outlook.com>
2021-12-06 11:18:07 -0500

    get put endpoint

--- api/server.js
+++ api/server.js
@@ -60,4 +60,24 @@ server.delete("/api/users/:id",async (req, res)=>{
     }
 })
 
+server.put("/api/users/:id",async (req,res) => {
+    const {id} = req.params
+    const changes = req.body
+    try{
+        if(!changes.name || !changes.bio){
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        }else{
+            const updatedUser = await User.update(id,changes)
+            if(!updatedUser){
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            }else{
+                res.status(200).json(updatedUser)
+            }
+        }    
+    }
+    catch(err){
+        res.status(500).json({ message: "The user information could not be modified" })
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
